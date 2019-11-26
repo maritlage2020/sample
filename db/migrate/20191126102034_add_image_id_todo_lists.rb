55be84df9475e65b139abc2249abf63023173d03
maritlage2020 <maritlage2020.amana@gmail.com>
2019-11-26 19:23:02 +0900

    [start]2-8

---
+++ db/migrate/20191126102034_add_image_id_todo_lists.rb
@@ -0,0 +1,4 @@
+class AddImageIdTodoLists < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_054307) do
+ActiveRecord::Schema.define(version: 2019_11_26_102034) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
