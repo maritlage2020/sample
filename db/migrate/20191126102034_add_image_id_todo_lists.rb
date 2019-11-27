11e90443946085e214e39166267c756def697a7a
maritlage2020 <maritlage2020.amana@gmail.com>
2019-11-27 17:29:44 +0900

    [finish]2-9

--- db/migrate/20191126102034_add_image_id_todo_lists.rb
+++
@@ -1,4 +0,0 @@
-class AddImageIdTodoLists < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20191126105045_add_image_id_to_todolists.rb
+++
@@ -1,5 +0,0 @@
-class AddImageIdToTodolists < ActiveRecord::Migration[5.2]
-  def change
-    add_column :todolists, :image_id, :string
-  end
-end
